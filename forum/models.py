e994622d64447800434c40e3373112862194f6e1
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-14 20:37:33 +0000

    Add forum app. Create Thread model.

---
+++ forum/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from news.models import Article
+
+
+# Create your models here.
+
+
+class Thread(models.Model):
+    """
+    Represents a thread in the forum
+    """
+    title = models.CharField(max_length=150, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="threads")
+    content = models.TextField(help_text="The main content of the thread.")
+    related_article = models.ForeignKey(Article, null=True, blank=True, on_delete=models.SET_NULL, related_name="related_threads", help_text="Optional: Link this thread to an article.")
+    related_article_url = models.URLField(max_length=250, blank=True, null=True, help_text="Optional: Paste the link to a related article.")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+            
+
