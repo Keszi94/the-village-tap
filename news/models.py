adfcdde51ec96b11020da7ed6f2d0eff1d14a5ae
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-14 15:56:37 +0000

    Create the Article model and the CATEGORY_CHOICES list.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+CATEGORY_CHOICES = [
+    ('news', 'General News'),
+    ('traffic', 'Traffic'),
+    ('obituaries', 'Obituaries'),
+    ('wedding', 'Wedding Announcements'),
+    ('history', 'Village History'),
+    ('achievements', 'Village Achievements'),
+]
+
+
+class Article(models.Model):
+    """
+    Represents a news article posted on the site.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True) # will be filled out automatically later
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='news')
+    blurb = models.TextField(help_text="A short summary of the article.")
+    content = models.TextField()
+    sources = models.TextField(
+        blank=True,
+        help_text="Optional: list any and all resources used in the article.")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False, help_text="Approved to post")
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.title    
