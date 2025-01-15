12b33f2644e09e638421edfdda54ec909f7d674b
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-15 19:05:32 +0000

    Create the index and base.html templates. Add a basic html structure for the home page.

--- news/views.py
+++ news/views.py
@@ -6,5 +6,6 @@ from .models import Article
 
 class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=2)
-    template_name = "article_list.html"
+    template_name = "news/index.html"
+    paginate_by = 12
 
