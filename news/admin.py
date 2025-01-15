0db44426a812f4021cd71e2a07b4e8d1f9e40894
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-15 00:13:48 +0000

    Add a new GUI menu to the Article textfield and add new filtering system to dashboard.

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Article
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Article)
+class NewsAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Article)
+# admin.site.register(Article)
