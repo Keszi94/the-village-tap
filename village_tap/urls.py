ceb03f07d88be4aaeb5aad02fd9c0af49c4a9177
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-18 00:17:38 +0000

    Install Allauth. Add the package to requirements.txt and the apps to settings.py.

--- village_tap/urls.py
+++ village_tap/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
     path('summernote/', include('django_summernote.urls')),
