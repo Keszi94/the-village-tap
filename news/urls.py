65e33691846899c8f6454ed783cba2512be3e0a0
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-19 18:53:44 +0000

    Bug fix: Fix the pathing issue in news/urls.py - wrong matching order

--- news/urls.py
+++ news/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('create/', views.create_article, name='create_article'),
     path('', views.article_list, name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
     path('category/<str:category>', views.article_list, name='articles_by_category'),
-    path('create/', views.create_article, name='create_article'),
     path('forum/', include('forum.urls')),
 ]
