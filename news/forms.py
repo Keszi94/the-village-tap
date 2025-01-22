877c2e440e4dee85ac4d9254440d9d7df1d56cd1
Eszter Kiskarpati <keszi94@gmail.com>
2025-01-22 00:33:13 +0000

    Add A GUI menu to the ArticleCreationForm using SummernoteWidget.

--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,8 @@
-from django import forms
 from .models import Article
+from django import forms
+from django_summernote.widgets import SummernoteWidget
 
-
+# not using crispy because I prefer not to
 class ArticleCreationForm(forms.ModelForm):
     class Meta:
         model = Article
@@ -11,7 +12,7 @@ class ArticleCreationForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'form-control'}),
             # using 'rows': to control the height of the text areas
             'blurb': forms.TextInput(attrs={'class': 'form-control', 'rows': 2}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'content': SummernoteWidget(attrs={'class': 'form-control', 'rows': 5}),
             'sources': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'status': forms.Textarea(attrs={'class': 'form-control'}),
         }
