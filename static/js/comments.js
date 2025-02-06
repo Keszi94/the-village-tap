190a43fc4b86cb295cf70578f4cb8271341a681b
Eszter Kiskarpati <keszi94@gmail.com>
2025-02-06 00:17:22 +0000

    Styling: Put Login/out/Register nav elements to the right side of navbar using bootstrap

--- static/js/comments.js
+++ static/js/comments.js
@@ -7,20 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
             let commentId = e.target.getAttribute("data-id");
             let commentContent = e.target.getAttribute("data-content");
 
-            // Get the form and textarea based on the comment ID with a unique identifier
             const commentEditForm = document.getElementById(`comment-edit-form-${commentId}`);
             const commentTextArea = document.getElementById(`comment-id_content-${commentId}`);
 
-            // Ensure the form and textarea are found
             if (commentEditForm && commentTextArea) {
-                // Set the comment content into the textarea for editing
+                // comment content for the textarea for editing
                 commentTextArea.value = commentContent;
 
                 // Toggle visibility of the form
                 if (commentEditForm.style.display === "block") {
-                    commentEditForm.style.display = "none"; // Hide the form if it's currently visible
+                    commentEditForm.style.display = "none"; // Hides the form if it's currently visible
                 } else {
-                    commentEditForm.style.display = "block"; // Show the form if it's hidden
+                    commentEditForm.style.display = "block"; // Shows the form if it's hidden
                 }
             } else {
                 console.error("Form or textarea not found for comment ID:", commentId);
