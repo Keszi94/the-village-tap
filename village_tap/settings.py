11f7649ac9ca2e442c621bd6f221e4f9e10e386d
Keszi94 <keszi94@gmail.com>
2025-05-26 19:03:18 +0100

    Bug fix: install widget-tweaks; update signup template and settings

--- village_tap/settings.py
+++ village_tap/settings.py
@@ -28,13 +28,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-
+# -----------------------------------------------------------
 # Application definition
-
+# -----------------------------------------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,14 +51,29 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'django_summernote',
     'django_extensions',
+    'widget_tweaks',
     'cloudinary',
+    # my apps
     'news',
     'forum',
 ]
 
 SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+# -----------------------------------------------------------
+# Allauth settings
+# -----------------------------------------------------------
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_SIGNUP_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -151,15 +166,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# -----------------------------------------------------------
 # Static files (CSS, JavaScript, Images)
+# -----------------------------------------------------------
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# -----------------------------------------------------------
 # Media files (uploaded images)
+# -----------------------------------------------------------
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
